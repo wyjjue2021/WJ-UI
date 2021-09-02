958cc377167c7f814369dcdcc67fdc3a9843c4fe
吴俊杰 <20717@etransfar.com>
2021-09-02 14:01:24 +0800

    Updates

--- .umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'dumi'
-
-export default defineConfig({
-    title:'WJ UI',
-    mode:'site',
-    outputPath: 'doc-site',
-    exportStatic:{},
-    dynamicImport:{}
-})
--- src/Button/demo/basic.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Button from '../index';
-import '../style';
-
-export default () => <Button>按钮</Button>;
