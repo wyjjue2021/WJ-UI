150450a280913ff885906d968c53e73e63199194
吴俊杰 <20717@etransfar.com>
2021-09-02 15:44:05 +0800

    环境隔离

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'dumi'
 
+let base = '/WJ-UI';
+let publicPath = '/WJ-UI/';
+
+if (process.env.SITE_BUILD_ENV === 'DEV') {
+  base = undefined;
+  publicPath = undefined;
+}
+
 export default defineConfig({
     title:'WJ UI',
     mode:'site',
     outputPath: 'doc-site',
     exportStatic:{},
-    dynamicImport:{}
+    dynamicImport:{},
+    base,
+    publicPath
 })
